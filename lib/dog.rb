9142c7e62032e320c5bb15b321ebbad8894b8291
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-05 18:18:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -61,7 +61,16 @@ class Dog
     self.new(attributes_hash)
   end
 
-  def self.find_or_create_by
+  def self.find_or_create_by(name:, breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?, breed = ?", name, breed)
+
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    else
+      dog = Dog.new(name: name, breed: breed)
+    end
+    dog
   end
 
   def self.find_by_name(name)
