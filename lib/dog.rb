6e541cf1c103e60afacef9122ca8cc763fa40849
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-05 18:25:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,11 @@ class Dog
   end
 
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ? AND breed = ?
+    SQL
+
+    DB[:conn].execute(sql, name, breed)
 
     if dog
       new_dog = self.new_from_db(dog)
