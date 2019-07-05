faa2e43cbedd9b5366ba0c895678edab3abfe4b0
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-05 18:26:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,7 +66,7 @@ class Dog
       SELECT * FROM dogs WHERE name = ? AND breed = ?
     SQL
 
-    DB[:conn].execute(sql, name, breed)
+    DB[:conn].execute(sql, name, breed).first
 
     if dog
       new_dog = self.new_from_db(dog)
