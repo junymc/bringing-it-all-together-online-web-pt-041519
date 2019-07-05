b0c46fde1e1955d649f0e9b749911b4e63997abb
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-05 17:16:14 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+
+  attr_accessor :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name =name
+    @breed = breed
+  end
+
+  def self.create_table
+    
+  end
+
+end
