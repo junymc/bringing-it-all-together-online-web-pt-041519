2fedaa883f83bfbc04b0c3223085b0c8e5900ee4
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-05 18:24:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,8 +68,8 @@ class Dog
       new_dog = self.new_from_db(dog)
     else
       new_dog = self.create(:name => name, :breed => breed)
-
-    dog
+    end
+     new_dog
   end
 
   def self.find_by_name(name)
